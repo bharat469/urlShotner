c7a7eb0a05ec5b3d3da01f0f5fd6ca4b6e41235d
bharat joshi <joshibharat469@gmail.com>
2026-07-04 10:53:36 +0530

    feat(added cors in app): :fire: cors(cross origin resource sharing)

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const dotenv = require('dotenv')
 const cors = require('cors')
 const connectDatabse = require('./config/db');
 const router = require('./routes/urlRoutes');
+
+const morgan = require("morgan");
+
 dotenv.config()
 connectDatabse().catch((e) =>
   console.log("unable to connect to mongo db server ", e),
 );
 const app = express()
 
+app.use(morgan("dev"));
+
 app.use(cors({ origin: 'http://localhost:5173' }))
 app.use(express.json())
+app.use(
+  cors({
+    url: "http://localhost:5173/",
+  }),
+);
 
 app.use('/api/urlShot',router)
 
